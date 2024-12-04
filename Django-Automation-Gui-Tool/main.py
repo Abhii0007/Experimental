65c159014abc4896de15f51f44457e354d4c8a11
Abhishek Verma <113354749+Abhii0007@users.noreply.github.com>
2024-12-04 21:54:02 +0530

    added custom Browser

--- Django-Automation-Gui-Tool/main.py
+++ Django-Automation-Gui-Tool/main.py
@@ -110,10 +110,10 @@ class window(QMainWindow):
         url = f"{txt}" + f"{text}"
         #os.system(f"start {url}")
         
-        window_x = window_name2()
+        window_x = window_designer()
         window_x.show()
-        window_x.form.lineEdit.setText(url)
-        window_x.form.webEngineView_5.setUrl(url)
+        window_x.form_designer.lineEdit.setText(url)
+        window_x.form_designer.webEngineView_5.setUrl(url)
         
     
     def clear(self):
@@ -139,39 +139,188 @@ class window(QMainWindow):
         #    print(f"Name: {user.name}, Phone: {user.phone}, Email: {user.email},password:{user.password}")
 
         print(pandas.DataFrame(list(users)))
+#---------------------------x----------------------------------
 
-
-        
-
-from window2 import Ui_designer
-class window_name2(QMainWindow):
+from PySide6.QtCore import Qt
+class window_designer(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.form = Ui_designer()
-        self.form.setupUi(self)
-        self.form.lineEdit.returnPressed.connect(self.search)
-        self.form.toolButton_aerrow_left.triggered.connect(lambda: self.form.webEngineView_5.page().triggerAction(self.form.webEngineView_5.page().WebAction.GoBack))
-        self.form.toolButton_aerrow_right.triggered.connect(lambda: self.form.webEngineView_5.page().triggerAction(self.form.webEngineView_5.page().WebAction.GoForward))
-        self.form.toolButton_refresh.triggered.connect(lambda: self.form.webEngineView_5.page().triggerAction(self.form.webEngineView_5.page().WebAction.ReloadAndBypassCache)) 
-        self.resizeEvent = self.resize_webview
+        from window2 import Ui_designer
+        from PySide6.QtWebEngineCore import ( QWebEngineSettings,QWebEngineProfile)
+        from PySide6.QtGui import QIcon
         
-    def search(self):
+        self.form_designer = Ui_designer()
+        self.form_designer.setupUi(self)
+        
+        self.resizeEvent = self.on_resize10
+        
+
+
+        self.form_designer.lineEdit.setVisible(True)
+        self.form_designer.lineEdit.move(125,1)
+        self.form_designer.widget_toolbar.setVisible(True)
+
+
+        self.form_designer.webEngineView_5.move(0,0)
+        
+        self.form_designer.widget_toolbar.move(0,0)
+        self.form_designer.toolButton_aerrow_left.setVisible(True)
+        self.form_designer.toolButton_aerrow_left.move(5,0)
+        self.form_designer.toolButton_aerrow_right.setVisible(True)
+        self.form_designer.toolButton_aerrow_right.move(30,0)
+        self.form_designer.toolButton_refresh.setVisible(True)
+        self.form_designer.toolButton_refresh.move(55,0)
+        self.form_designer.pushButton_go.setVisible(True)
+        self.form_designer.pushButton_go.move(80,0)
+        self.form_designer.pushButton_save_page.setVisible(True)
+        self.form_designer.pushButton_maximise.setVisible(True)
+        self.form_designer.pushButton_go.clicked.connect(self.opener)
+
+
+        self.form_designer.lineEdit.setStyleSheet("""
+            QLineEdit {
+                border: 2px solid rgb(0, 0, 0); /* Custom border color */
+                border-radius: 8px; /* Optional: Make the corners rounded */
+                
+                                          
+                background-color: rgb(240, 240, 240); /* Background color */
+                alternate-background-color: rgb(100, 100, 100); /* Alternate background color */
+                selection-background-color: rgb(137, 143, 255); /* Selected text background color */
+                selection-color: rgb(0, 0, 0); /* Selected text color */
+                color: rgb(0, 0, 0); /* Text color */
+            }
+        """)
+
+
+       
+        self.form_designer.lineEdit.returnPressed.connect(self.navigate)
+    
+        
+
+        
+
+
+        #self.settings = self.form_designer.webEngineView_5.page().profile()
+        #self.settings.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
+        
+     
+
+        self.profile = self.form_designer.webEngineView_5.page().profile()
+        self.profile.settings().setAttribute(QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard, True)
+        self.profile.settings().setAttribute(QWebEngineSettings.WebAttribute.JavascriptCanPaste, True)
+        #profile.downloadRequested.connect(self.on_downloadRequested)
+
+        #self.form_designer.webEngineView_5.setPage(CustomWebEnginePage(self))
+        self.form_designer.webEngineView_5.setZoomFactor(0.8)
+       
+        profile1 = QWebEngineProfile.defaultProfile()
+        profile1.setHttpUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
+
+
+
+
+        self.form_designer.toolButton_aerrow_left.clicked.connect(self.form_designer.webEngineView_5.back)
+        self.form_designer.toolButton_aerrow_right.clicked.connect(self.form_designer.webEngineView_5.forward)
+        self.form_designer.toolButton_refresh.clicked.connect(self.form_designer.webEngineView_5.reload)    
+        
+        self.form_designer.pushButton_maximise.clicked.connect(self.fulldisplay)
+        self.fullscreen = False
+
+
+
+    def fulldisplay(self):
+        
+        if self.fullscreen:
+            self.showNormal()
+            self.fullscreen = False
+        else:
+            self.showFullScreen()
+            self.fullscreen = True
+
+
+
+
+    def enterEvent(self, event):
+        
+        # Focus the window when the mouse enters
+        self.activateWindow()  # Bring window to the front
+        self.setFocus(Qt.OtherFocusReason)  # Set focus explicitly
+        super().enterEvent(event)
+
+    def opener(self):
+        my_url = self.form_designer.webEngineView_5.url().toString()
+        os.system(f"start {my_url}")
+
+
+
+
+    '''def on_downloadRequested(self, download: QWebEngineDownloadRequest):
+        # Open file dialog to select download location
+        file_path, _ = QFileDialog.getSaveFileName(self, "Save File", download.suggestedFileName())
+        
+        if file_path:
+            # Print debug info
+            print(f"Download path selected: {file_path}")
+            directory = QFileInfo(file_path).absolutePath()
+            file_name = QFileInfo(file_path).fileName()
+
+            print(f"Directory: {directory}")
+            print(f"File Name: {file_name}")
+
+            if QDir(directory).exists():
+                print("Directory exists, proceeding with download.")
+            else:
+                print("Directory does not exist, creating directory.")
+                if QDir().mkpath(directory):
+                    print("Directory created successfully.")
+                else:
+                    print("Failed to create directory.")
+                    QMessageBox.critical(self, "Error", f"Failed to create directory: {directory}")
+                    return
+            
+            # Set the download directory and file name
+            download.setDownloadDirectory(directory)
+            download.setDownloadFileName(file_name)
+            download.accept()
+
+            print("Download accepted.")
+        else:
+            download.cancel()
+            print("Download was canceled by the user.")'''
+
+       
+
+
+    def navigate(self):
+        self.form_designer.webEngineView_5.setUrl(self.form_designer.lineEdit.text())
+        
+
+
+    def on_resize10(self, event):
+        x,y = self.geometry().width(),self.geometry().height()
+        self.form_designer.webEngineView_5.setGeometry(1,23,x-3,y-26)
+        self.form_designer.widget_toolbar.setGeometry(0,0,x-2,23)
+        self.form_designer.lineEdit.setGeometry(130,0,x-260,22)
+        self.form_designer.pushButton_save_page.setGeometry(x-130,1,22,22)
+        self.form_designer.pushButton_maximise.setGeometry(x-105,-1,22,22)
+    
+
+
+        
+    '''def search(self):
         url = self.form.lineEdit.text()
         if not url.startswith('http://') and not url.startswith('https://'):
             url = 'http://' + url
-        self.form.webEngineView_5.setUrl(url)
+        self.form.webEngineView_5.setUrl(url)'''
 
     
 
-    def resize_webview(self, event):
-        self.form.webEngineView_5.resize(self.width(), self.height())
-
+   
 
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     widget = window()
     widget.show()
-    
     
     sys.exit(app.exec())
--- Django-Automation-Gui-Tool/window2.py
+++ Django-Automation-Gui-Tool/window2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'Form2OQvFns.ui'
+## Form generated from reading UI file 'Form2MdPoqw.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.1
 ##
@@ -23,7 +23,7 @@ class Ui_designer(object):
     def setupUi(self, designer):
         if not designer.objectName():
             designer.setObjectName(u"designer")
-        designer.resize(1000, 700)
+        designer.resize(690, 390)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -36,12 +36,23 @@ class Ui_designer(object):
 "background-color: rgb(0, 0, 0);")
         self.webEngineView_5 = QWebEngineView(designer)
         self.webEngineView_5.setObjectName(u"webEngineView_5")
-        self.webEngineView_5.setGeometry(QRect(0, 22, 1000, 700))
+        self.webEngineView_5.setGeometry(QRect(2, 2, 686, 386))
         self.webEngineView_5.setUrl(QUrl(u"https://www.google.co.in/"))
         self.webEngineView_5.setZoomFactor(0.800000000000000)
+        self.lineEdit = QLineEdit(designer)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(64, 36, 1743, 20))
+        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
+        self.lineEdit.setSizePolicy(sizePolicy)
+        font = QFont()
+        font.setPointSize(12)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setStyleSheet(u"background-color: rgba(0, 0, 0,0.8);\n"
+"color: rgb(255, 255, 255);")
+        self.lineEdit.setFrame(False)
         self.toolButton_aerrow_left = QToolButton(designer)
         self.toolButton_aerrow_left.setObjectName(u"toolButton_aerrow_left")
-        self.toolButton_aerrow_left.setGeometry(QRect(6, -1, 22, 22))
+        self.toolButton_aerrow_left.setGeometry(QRect(379, 5, 22, 22))
         self.toolButton_aerrow_left.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.toolButton_aerrow_left.setStyleSheet(u"background-color: rgba(0, 0, 0, 0);")
         icon = QIcon()
@@ -52,7 +63,7 @@ class Ui_designer(object):
         self.toolButton_aerrow_left.setArrowType(Qt.ArrowType.NoArrow)
         self.toolButton_aerrow_right = QToolButton(designer)
         self.toolButton_aerrow_right.setObjectName(u"toolButton_aerrow_right")
-        self.toolButton_aerrow_right.setGeometry(QRect(31, -1, 22, 22))
+        self.toolButton_aerrow_right.setGeometry(QRect(404, 5, 22, 22))
         self.toolButton_aerrow_right.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.toolButton_aerrow_right.setStyleSheet(u"background-color: rgba(0, 0, 0, 0);")
         icon1 = QIcon()
@@ -63,7 +74,7 @@ class Ui_designer(object):
         self.toolButton_aerrow_right.setArrowType(Qt.ArrowType.NoArrow)
         self.pushButton_go = QToolButton(designer)
         self.pushButton_go.setObjectName(u"pushButton_go")
-        self.pushButton_go.setGeometry(QRect(83, -1, 22, 22))
+        self.pushButton_go.setGeometry(QRect(456, 5, 22, 22))
         self.pushButton_go.setStyleSheet(u"background-color: rgba(0, 0, 0, 0);")
         icon2 = QIcon()
         icon2.addFile(u"../../../Octopus v1.71/images/redirect.ico", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
@@ -73,7 +84,7 @@ class Ui_designer(object):
         self.pushButton_go.setArrowType(Qt.ArrowType.NoArrow)
         self.toolButton_refresh = QToolButton(designer)
         self.toolButton_refresh.setObjectName(u"toolButton_refresh")
-        self.toolButton_refresh.setGeometry(QRect(56, -2, 25, 25))
+        self.toolButton_refresh.setGeometry(QRect(429, 4, 25, 25))
         self.toolButton_refresh.setStyleSheet(u"background-color: rgba(0, 0, 0, 0);")
         icon3 = QIcon()
         icon3.addFile(u"../../../Octopus v1.71/images/refresh.ico", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
@@ -83,11 +94,11 @@ class Ui_designer(object):
         self.toolButton_refresh.setArrowType(Qt.ArrowType.NoArrow)
         self.widget_toolbar = QWidget(designer)
         self.widget_toolbar.setObjectName(u"widget_toolbar")
-        self.widget_toolbar.setGeometry(QRect(0, 0, 1000, 22))
+        self.widget_toolbar.setGeometry(QRect(373, 6, 211, 22))
         self.widget_toolbar.setStyleSheet(u"background-color: rgb(32, 32, 32);")
         self.pushButton_maximise = QToolButton(self.widget_toolbar)
         self.pushButton_maximise.setObjectName(u"pushButton_maximise")
-        self.pushButton_maximise.setGeometry(QRect(107, 0, 22, 22))
+        self.pushButton_maximise.setGeometry(QRect(160, 0, 22, 22))
         self.pushButton_maximise.setStyleSheet(u"background-color: rgba(0, 0, 0, 0);")
         icon4 = QIcon()
         icon4.addFile(u"../../../Octopus v1.71/images/maximise.ico", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
@@ -95,17 +106,24 @@ class Ui_designer(object):
         self.pushButton_maximise.setIconSize(QSize(20, 20))
         self.pushButton_maximise.setPopupMode(QToolButton.ToolButtonPopupMode.DelayedPopup)
         self.pushButton_maximise.setArrowType(Qt.ArrowType.NoArrow)
-        self.lineEdit = QLineEdit(self.widget_toolbar)
-        self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setGeometry(QRect(136, 0, 721, 22))
-        self.lineEdit.setStyleSheet(u"background-color: rgb(227, 227, 227);\n"
-"color: rgb(0, 0, 0);")
+        self.pushButton_save_page = QToolButton(designer)
+        self.pushButton_save_page.setObjectName(u"pushButton_save_page")
+        self.pushButton_save_page.setGeometry(QRect(502, 7, 22, 22))
+        self.pushButton_save_page.setStyleSheet(u"background-color: rgba(0, 0, 0, 0);")
+        icon5 = QIcon()
+        icon5.addFile(u"../../../Octopus v1.71/images/save_page.ico", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.pushButton_save_page.setIcon(icon5)
+        self.pushButton_save_page.setIconSize(QSize(20, 20))
+        self.pushButton_save_page.setPopupMode(QToolButton.ToolButtonPopupMode.DelayedPopup)
+        self.pushButton_save_page.setArrowType(Qt.ArrowType.NoArrow)
         self.widget_toolbar.raise_()
         self.webEngineView_5.raise_()
         self.toolButton_aerrow_left.raise_()
         self.toolButton_aerrow_right.raise_()
         self.toolButton_refresh.raise_()
         self.pushButton_go.raise_()
+        self.lineEdit.raise_()
+        self.pushButton_save_page.raise_()
 
         self.retranslateUi(designer)
 
@@ -114,6 +132,8 @@ class Ui_designer(object):
 
     def retranslateUi(self, designer):
         designer.setWindowTitle(QCoreApplication.translate("designer", u"Form", None))
+        self.lineEdit.setText("")
+        self.lineEdit.setPlaceholderText(QCoreApplication.translate("designer", u"Enter Custom URL in this Format ex :-    https://www.google.com/search?q={query}", None))
 #if QT_CONFIG(tooltip)
         self.toolButton_aerrow_left.setToolTip(QCoreApplication.translate("designer", u"<html><head/><body><p>Go to Previous Page</p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
@@ -134,5 +154,9 @@ class Ui_designer(object):
         self.pushButton_maximise.setToolTip(QCoreApplication.translate("designer", u"<html><head/><body><p>FullScreen / Normal</p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
         self.pushButton_maximise.setText("")
+#if QT_CONFIG(tooltip)
+        self.pushButton_save_page.setToolTip(QCoreApplication.translate("designer", u"<html><head/><body><p>Save Current Page</p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_save_page.setText("")
     # retranslateUi
 
